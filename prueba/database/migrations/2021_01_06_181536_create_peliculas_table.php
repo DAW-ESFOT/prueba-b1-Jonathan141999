aabba0d197896955261253c4ca2ce13a3afdfbcc
Jona141999 <jona94alquinga@gmail.com>
2021-01-06 18:23:32 -0500

    Creacion de Modelos

--- prueba/database/migrations/2021_01_06_181536_create_peliculas_table.php
+++ prueba/database/migrations/2021_01_06_181536_create_peliculas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePeliculasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('peliculas', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name',255);
-            $table->string('lastname',255);
-            $table->string('document',80);
-            $table->rememberToken();
+            $table->string('code',80);
+            $table->double('year');
+            $table->boolean('available');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('peliculas');
     }
 }
